c793588bc2b50cd8975e4ff67e2e826f4add64c0
Matthew Ragan <raganmd@gmail.com>
2026-03-22 22:49:30 -0700

    logging utilities update

--- src/td_builder/logging_utils.py
+++ src/td_builder/logging_utils.py
@@ -12,11 +12,12 @@ def log_event(msg: str, indent: int = 0, isError: bool = False) -> None:
 def write_log_to_cloud(log_path: str) -> None:
     '''A function that reads out the lines of a log file one at a time so it can be shared with the cloud
     '''
+    log_event(msg=f"attempting to open {log_path}")
     try:
         with open(log_path, 'r') as target_file:
             for line in target_file:
                 # Print each line
-                print(f"--> {line.strip()}")
+                print(f"{line.strip()}")
 
         # deletes file after reading it
         log_event(msg="Cleaning up log", indent=1)
